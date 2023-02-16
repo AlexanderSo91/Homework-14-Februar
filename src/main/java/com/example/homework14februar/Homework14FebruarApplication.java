22a1e97d24c2ea86941f31d16a703d185a000725
zotin <zotinalexander@gmail.com>
2023-02-16 19:20:52 +0100

    commit

---
+++ src/main/java/com/example/homework14februar/Homework14FebruarApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework14februar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework14FebruarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework14FebruarApplication.class, args);
+    }
+
+}
